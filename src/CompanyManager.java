e1b5f4181406691fb81687477c56e31109a35728
Nabz786 <nabvali98@gmail.com>
2018-01-08 11:40:18 -0500

    Started constructing basic classes for the program

---
+++ src/CompanyManager.java
@@ -0,0 +1,12 @@
+public class CompanyManager {
+//This class will initially house very simple operations such as hire and fire
+//More operations will be added later.
+
+private void hire(){
+
+}
+
+private void fire(){
+
+}
+}
