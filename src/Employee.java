4a25ec39c9ca07f615c31c87a691b1ddd422c2c8
Nabz786 <nabvali98@gmail.com>
2018-01-09 09:49:01 -0500

    Added basic vars to employee class

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,22 @@
 public class Employee {
-    
+
+private String firstName, lastName;
+private int age;
+
+
+public Employee(){
+
+}
+
+public Employee(String firstName, String lastName, int age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+}
+
+
+
+
+
+
 }
