e15bc5ec6a0b6ab7c88874aa923a5fdf6a11ea05
Nabz786 <nabvali98@gmail.com>
2018-01-10 11:20:04 -0500

    Continued work on various classes

---
+++ src/Manager.java
@@ -0,0 +1,5 @@
+public class Manager extends Employee{
+    public Manager(String fName, String lName, int empAge, int empId, EmployeePosition employeePosition) {
+        super(fName, lName, empAge, empId, employeePosition);
+    }
+}
--- src/SalesRep.java
+++ src/SalesRep.java
@@ -3,10 +3,4 @@ public class SalesRep extends Employee {
     public SalesRep(String fName, String lName, int empAge, int empId, EmployeePosition employeePosition) {
         super(fName, lName, empAge, empId, employeePosition);
     }
-
-
-    public static void main(String[] args) {
-        SalesRep s = new SalesRep("John", "Smith", 18, 353345, EmployeePosition.SalesRep);
-        System.out.print(s.getEmployeePosition());
-    }
 }
