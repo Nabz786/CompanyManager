c0581c61a14ca6eb5ed5009fd174fb4d33586ff9
Nabz786 <nabvali98@gmail.com>
2018-01-11 17:49:43 -0500

    Simplified and continued work on classes

--- src/CompanyManager.java
+++ src/CompanyManager.java
@@ -1,12 +1,6 @@
 public class CompanyManager {
-//This class will initially house very simple operations such as hire and fire
-//More operations will be added later.
 
-private void hire(){
 
-}
 
-private void fire(){
 
-}
 }
--- src/SalesRep.java
+++ src/SalesRep.java
@@ -3,4 +3,8 @@ public class SalesRep extends Employee {
     public SalesRep(String fName, String lName, int empAge, int empId, EmployeePosition employeePosition) {
         super(fName, lName, empAge, empId, employeePosition);
     }
+
+    private void contactManager(String message){
+
+    }
 }
