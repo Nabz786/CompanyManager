52a2de6bd93eeebd203fc15cf122cafd26f4f51c
Nabz786 <nabvali98@gmail.com>
2018-01-11 17:57:46 -0500

    Started work on company management side of things ex. hire and fire

--- src/CompanyManager.java
+++ src/CompanyManager.java
@@ -1,4 +1,30 @@
+import java.awt.event.ComponentAdapter;
+import java.util.ArrayList;
+
 public class CompanyManager {
+
+    private ArrayList<Employee> employeeList;
+
+    public CompanyManager(){
+        employeeList = new ArrayList<>();
+    }
+
+    private void hireEmployee(Employee employee){
+        employeeList.add(employee);
+    }
+
+    private void fireEmployee(Employee employee){
+        employeeList.remove(employee);
+    }
+
+
+    public static void main(String[] args) {
+        CompanyManager comp = new CompanyManager();
+        comp.hireEmployee(new SalesRep("John", "Smith", 17, 324324, EmployeePosition.SalesRep));
+        System.out.println(comp.employeeList.get(0).getEmpAge());
+        comp.fireEmployee(comp.employeeList.get(0));
+        System.out.print(comp.employeeList.size());
+    }
 
 
 
