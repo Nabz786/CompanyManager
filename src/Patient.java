c60eea85c2f94773c5de6b14505e5e1b201bd190
Nabz786 <nabvali98@gmail.com>
2018-01-17 11:23:55 -0500

    Added the patients current location

--- src/Patient.java
+++ src/Patient.java
@@ -1,24 +1,24 @@
-import javafx.scene.shape.PathElement;
-
 import java.util.Date;
 
 public class Patient {
 
     private String fName, lName;
     private Date dateOfBirth;
-    private illnessDepartment illnessDepartment;
+    private illnessDepartment illness;
+    private PatientLocation patientLocation;
     private int patientID;
 
-    public Patient(String fName, String lName, Date dateOfBirth, illnessDepartment illnessDepartment,
+    public Patient(String fName, String lName, Date dateOfBirth, illnessDepartment illness,
     int patientID) {
         this.fName = fName;
         this.lName = lName;
         this.dateOfBirth = dateOfBirth;
-        this.illnessDepartment = illnessDepartment;
+        this.illness = illness;
         this.patientID = patientID;
+        this.patientLocation = new PatientLocation(illnessDepartment.Receiving, 000);
     }
 
     private void setIllnessDepartment(illnessDepartment illnessDepartment){
-        this.illnessDepartment = illnessDepartment;
+        this.illness = illnessDepartment;
     }
 }
