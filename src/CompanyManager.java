cf04f66f55975d7b79849f8f857c3e89741c2b0b
Nabz786 <nabvali98@gmail.com>
2018-01-17 08:48:41 -0500

    un-important changes for now

--- src/CompanyManager.java
+++ src/CompanyManager.java
@@ -19,9 +19,12 @@ public class CompanyManager {
 
     public static void main(String[] args) {
         CompanyManager comp = new CompanyManager();
-        System.out.println(comp.employeeList.get(0).getEmpAge());
-        comp.fireEmployee(comp.employeeList.get(0));
-        System.out.print(comp.employeeList.size());
+        //TODO: Figure out how to implement date, for DOB
+
+        //Patient p = new Patient("John", "Smith", 1/9/98, illnessDepartment.Cardiovascular);
+        //System.out.println(comp.employeeList.get(0).getEmpAge());
+        //comp.fireEmployee(comp.employeeList.get(0));
+        //System.out.print(comp.employeeList.size());
     }
 
 
