9fcdaca5fc634e892d7fa8c65b6f9753f1f62154
Nabz786 <nabvali98@gmail.com>
2018-01-17 08:51:13 -0500

    added more content to patient class

--- src/Patient.java
+++ src/Patient.java
@@ -7,13 +7,18 @@ public class Patient {
     private String fName, lName;
     private Date dateOfBirth;
     private illnessDepartment illnessDepartment;
+    private int patientID;
 
-    public Patient(String fName, String lName, Date dateOfBirth, illnessDepartment illnessDepartment) {
+    public Patient(String fName, String lName, Date dateOfBirth, illnessDepartment illnessDepartment,
+    int patientID) {
         this.fName = fName;
         this.lName = lName;
         this.dateOfBirth = dateOfBirth;
         this.illnessDepartment = illnessDepartment;
+        this.patientID = patientID;
     }
 
-
+    private void setIllnessDepartment(illnessDepartment illnessDepartment){
+        this.illnessDepartment = illnessDepartment;
+    }
 }
