9a571a852d10dd5f637ee2b462dc4c7ad8dbef9e
Nabz786 <nabvali98@gmail.com>
2018-01-17 13:52:42 -0500

    Added a doctor class for doctors

---
+++ src/Doctor.java
@@ -0,0 +1,5 @@
+public class Doctor extends Employee {
+    public Doctor(String fName, String lName, int empAge, int empId, double empSalary, EmployeePosition employeePosition) {
+        super(fName, lName, empAge, empId, empSalary, employeePosition);
+    }
+}
