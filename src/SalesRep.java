e12ab3da805b89a9b322c50cafbc64e46b0e5234
Nabeel V <nabvali98@gmail.com>
2018-01-16 19:16:13 -0500

    Delete SalesRep.java

--- src/SalesRep.java
+++
@@ -1,6 +0,0 @@
-public class SalesRep extends Employee {
-
-    public SalesRep(String fName, String lName, int empAge, int empId, double empSalary, EmployeePosition empPosition) {
-        super(fName, lName, empAge, empId, empSalary, empPosition);
-    }
-}
