64e5965ba417bdf914e1914d06209c7bc614c6ed
Nabz786 <nabvali98@gmail.com>
2018-01-18 09:49:23 -0500

    Added the ability to assign a doctor and a nurse to a patient

--- src/Patient.java
+++ src/Patient.java
@@ -2,23 +2,36 @@ import java.util.Date;
 
 public class Patient {
 
-    private String fName, lName;
+    private String fName, lName, Symptoms;
     private Date dateOfBirth;
     private illnessDepartment illness;
     private PatientLocation patientLocation;
     private int patientID;
+    private Nurse assignedNurse;
+    private Doctor assignedDoctor;
+
 
     public Patient(String fName, String lName, Date dateOfBirth, illnessDepartment illness,
-    int patientID) {
+    int patientID, String symptoms) {
         this.fName = fName;
         this.lName = lName;
         this.dateOfBirth = dateOfBirth;
         this.illness = illness;
         this.patientID = patientID;
-        this.patientLocation = new PatientLocation(illnessDepartment.Receiving, 000);
+        this.Symptoms = symptoms;
+        this.patientLocation = new PatientLocation(illnessDepartment.Reception, 000);
     }
 
     private void setIllnessDepartment(illnessDepartment illnessDepartment){
         this.illness = illnessDepartment;
     }
+
+    private void setAssignedNurse(Nurse nurse){
+        this.assignedNurse = nurse;
+    }
+
+    private void setAssignedDoctor(Doctor doctor){
+        this.assignedDoctor = doctor;
+    }
+
 }
