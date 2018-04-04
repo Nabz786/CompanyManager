fdc91e2144e835d1c28ca738253c70bb62b57d9b
Nabz786 <nabvali98@gmail.com>
2018-04-04 10:31:57 -0400

    created package to hold main hospital managing engine

---
+++ src/hmanager/illnessDepartment.java
@@ -0,0 +1,27 @@
+package hmanager;
+
+public enum illnessDepartment {
+    Cardiology,
+    EarNoseThroat,
+    Geriatric,
+    Gastroenterology,
+    Gynaecology,
+    Haematology,
+    Pediatrics,
+    Neurology,
+    Oncology,
+    Opthamology,
+    Orthopedics,
+    Urology,
+    Pyschiatry,
+
+    //Below are the various locations in a hospital
+    EmergencyRoom,
+    Anaesthetics,
+    IntensiveCare,
+    Surgery,
+    MRI,
+    Catscan,
+    Reception,
+    Xray
+}
