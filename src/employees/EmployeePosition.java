8acd49477640fd5f76db6d6627471833655e0fed
Nabz786 <nabvali98@gmail.com>
2018-04-06 10:32:53 -0400

    Added Comments

--- src/employees/EmployeePosition.java
+++ src/employees/EmployeePosition.java
@@ -1,9 +1,22 @@
 package employees;
 
+/**
+ * The various positions within the hospital as of now,
+ * this will continue to be updated
+ */
 public enum EmployeePosition{
+    /** Nurse Position.**/
     Nurse,
+
+    /** Doctor Position.**/
     Doctor,
+
+    /** Surgeon Position.**/
     Surgeon,
-    LabTechnichian,
+
+    /** LabTechnician Position .**/
+    LabTechnician,
+
+    /** Custodian Position.**/
     Custodian
 }
