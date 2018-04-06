d1184462d21dc4583f93b39a6c0dbd5f8362b0f5
Nabz786 <nabvali98@gmail.com>
2018-04-06 10:33:13 -0400

    Implemented assign/un-assign patient ability

--- src/employees/Nurse.java
+++ src/employees/Nurse.java
@@ -2,10 +2,21 @@ package employees;
 
 import employees.Employee;
 import employees.EmployeePosition;
+import patients.Patient;
 
 public class Nurse extends Employee implements Caretaker {
 
     public Nurse(String fName, String lName, int empAge, int empId, double empSalary, EmployeePosition employeePosition) {
         super(fName, lName, empAge, empId, empSalary, employeePosition);
+    }
+
+    @Override
+    public void assignPatient(Patient p) {
+        myPatients.add(p);
+    }
+
+    @Override
+    public void removePatient(Patient p) {
+        myPatients.add(p);
     }
 }
