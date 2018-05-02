69a34a372bac30408964a9c0cccd6a338241fb20
Nabz786 <nabvali98@gmail.com>
2018-05-02 15:51:41 -0400

    Removed redundant empPosition param

--- src/employees/Nurse.java
+++ src/employees/Nurse.java
@@ -6,17 +6,17 @@ import patients.Patient;
 
 public class Nurse extends Employee implements Caretaker {
 
-    public Nurse(String fName, String lName, int empAge, int empId, double empSalary, EmployeePosition employeePosition) {
-        super(fName, lName, empAge, empId, empSalary, employeePosition);
+    public Nurse(String fName, String lName, int empAge, int empId, double empSalary) {
+        super(fName, lName, empAge, empId, empSalary, EmployeePosition.Nurse);
     }
 
     @Override
     public void assignPatient(Patient p) {
-        myPatients.add(p);
+        patientList.add(p);
     }
 
     @Override
     public void removePatient(Patient p) {
-        myPatients.add(p);
+        patientList.add(p);
     }
 }
