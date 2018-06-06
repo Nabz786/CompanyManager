7c3bb7b1c2d4dd75bd5906ebbc05a40a8e879413
Nabz786 <nabvali98@gmail.com>
2018-06-05 20:54:43 -0400

    Added ability to pass SQL queries to this class instead of having to repeatedly create connections to database inside patient class

--- src/hmanager/DatabaseConnectionManager.java
+++ src/hmanager/DatabaseConnectionManager.java
@@ -1,9 +1,6 @@
 package hmanager;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class DatabaseConnectionManager {
 
@@ -12,11 +9,19 @@ public class DatabaseConnectionManager {
         Class.forName(driver);
         String url = "jdbc:mysql://localhost:3306/hospitalschema?autoReconnect=true&useSSL=false\r\n";
         String username = "root";
-        String password = ""; //replace password here
+        String password = "nabza340"; //replace password here
 
         Connection conn = DriverManager.getConnection(url, username,password);
         System.out.println("Connected");
         return conn;
+    }
+
+    public static void executeStatement(final String sqlStatement) throws Exception {
+        Connection con = getConnection();
+        Statement statement = con.createStatement();
+        statement.executeUpdate(sqlStatement);
+        con.close();
+
     }
 }
 
