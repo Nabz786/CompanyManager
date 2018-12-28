a6cd05a6a46d55a475115ae6d84d955e88d7de26
Nabeel V <nabvali98@gmail.com>
2018-12-27 22:04:31 -0500

    Improved error handleing within services to minimize the need to throw exceptions within the UI classes

--- src/Services/DashboardCountService.java
+++ src/Services/DashboardCountService.java
@@ -1,5 +1,6 @@
 package Services;
 
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -7,40 +8,54 @@ import java.sql.SQLException;
 public class DashboardCountService {
 
 
-    public DashboardCountService() throws Exception{}
+    public DashboardCountService() {}
     /**
      * Returns the number of patients currently in the hospital
      */
-    public int getPersonCounts() throws Exception {
+    public int getPersonCounts() {
         int patientCount = 0;
         String sqlStatement = "SELECT count(*)  AS NumPatients FROM Patient";
         Connection dbConnection = DatabaseConnectionService.getConnection();
-        ResultSet resultSet = dbConnection.createStatement().executeQuery(sqlStatement);
-        while(resultSet.next()) {
-            patientCount = resultSet.getInt("NumPatients");
+        try {
+            ResultSet resultSet = dbConnection.createStatement().executeQuery(sqlStatement);
+            if (resultSet.next()) {
+                patientCount = resultSet.getInt("NumPatients");
+            }
+        } catch (Exception e){
+            System.out.println(e);
         }
         return patientCount;
     }
 
-    public int getNurseCounts() throws Exception {
+    public int getNurseCounts() {
         int nurseCount = 0;
         String sqlStatement = "SELECT count(*)  AS NumNurse FROM Nurse";
         Connection dbConnection = DatabaseConnectionService.getConnection();
-        ResultSet resultSet = dbConnection.createStatement().executeQuery(sqlStatement);
-        while(resultSet.next()) {
-            nurseCount = resultSet.getInt("NumNurse");
+        try {
+            ResultSet resultSet = dbConnection.createStatement().executeQuery(sqlStatement);
+            if (resultSet.next()) {
+                nurseCount = resultSet.getInt("NumNurse");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
+
         return nurseCount;
     }
 
-    public int getDoctorCounts() throws Exception {
+    public int getDoctorCounts() {
         int doctorCount = 0;
         String sqlStatement = "SELECT count(*)  AS NumDoctor FROM Doctor";
         Connection dbConnection = DatabaseConnectionService.getConnection();
-        ResultSet resultSet = dbConnection.createStatement().executeQuery(sqlStatement);
-        while(resultSet.next()) {
-            doctorCount = resultSet.getInt("NumDoctor");
+        try {
+            ResultSet resultSet = dbConnection.createStatement().executeQuery(sqlStatement);
+            if (resultSet.next()) {
+                doctorCount = resultSet.getInt("NumDoctor");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
         }
+
         return doctorCount;
     }
 }
--- src/Services/DatabaseConnectionService.java
+++ src/Services/DatabaseConnectionService.java
@@ -1,18 +1,23 @@
-package hmanager;
+package Services;
 
 import java.sql.*;
 
-public class DatabaseConnectionManager {
+public class DatabaseConnectionService {
 
-    public static Connection getConnection() throws Exception {
-        String driver = "com.mysql.cj.jdbc.Driver";
-        Class.forName(driver);
-        String url = "jdbc:mysql://nvinstance.cli0ucsmllzj.us-east-2.rds.amazonaws.com:3306/HospitalManagementProject";
-        String username = "nabz_master";
-        String password = "abc12345"; //replace password here
-        Connection conn = DriverManager.getConnection(url, username, password);
-        System.out.println("Connected");
-        return conn;
+    public static Connection getConnection() {
+        try {
+            String driver = "com.mysql.cj.jdbc.Driver";
+            Class.forName(driver);
+            String url = "jdbc:mysql://nvinstance.cli0ucsmllzj.us-east-2.rds.amazonaws.com:3306/HospitalManagementProject";
+            String username = "nabz_master";
+            String password = "abc12345"; //replace password here
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connected");
+            return conn;
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        return null;
     }
 
     public static void executeStatement(final String sqlStatement) throws Exception {
